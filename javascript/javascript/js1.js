92d8db7ae6ff6780caf86c37ca42a994285851c0
Nitin201 <dodamanin8@gmail.com>
2023-08-24 12:13:31 +0530

    commit

---
+++ javascript/javascript/js1.js
@@ -0,0 +1,55 @@
+/*function avg(a, b) {    
+    return (a+b)/2;
+}
+c1=avg(4,5);
+c2=avg(8,6);
+console.log(c1,c2);
+document.write('i want to get back as strongas lion'
+)*/
+//for loop
+
+/*var arr = [1, 2, 3, 4, 5, 6, 7];
+for(var i=0;i<arr.length;i++)
+{
+   console.log(arr[i]);
+}*/
+
+/*arr.forEach(function (element)
+{
+    console.log(element);
+
+})
+
+//while loop
+let j=0;
+while(j<arr.length)
+{
+    console.log(arr[j]);
+    j++;
+}
+
+//dowhile
+do{
+    console.log(arr[j]);
+}while(j<arr.length);*/
+/*console.log(arr.length);
+arr.pop();
+arr.push("nitin");
+arr.reverse();
+arr.shift();
+arr.unshift("nitin");
+console.log(arr);*/
+
+
+
+//strings
+
+var mystring="hello nitin he is good boy";
+console.log( mystring.length);
+d=mystring.replace("nitin","sunita");
+console.log(d);
+
+//DATE
+let date=new Date();
+console.log( date.getFullYear(), date.getMonth(),date.getTime());
+
